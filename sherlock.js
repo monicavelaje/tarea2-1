5147862cf0c55c70ce941fe46b54a7bf1a5c5fba
Monés <monica.velaje@udlap.mx>
2015-10-02 00:29:20 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,39 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+public class Sherlock {
+    
+    public static void main(String[] args) {
+        
+        String s = "2\n3 9\n17 24";
+        String[] split = s.split("\\n");
+        for (int i = 1; i <= Integer.parseInt(split[0]); i++) {
+            String[] interval = split[i].split("\\ ");
+            int a = Integer.parseInt(interval[0]);
+            int b = Integer.parseInt(interval[1]);
+            int roots = getNumberOfRoots(a, b);
+            System.out.println();
+            System.out.println("Interval " + a + " to "+ b + " has " + roots + " roots");
+        }
+        
+    }
+    
+    private static int getNumberOfRoots(int a, int b) {
+        int count = 0;
+        if (b < a) { //por si el intervalo está escrito al revés
+            final int temp = a;
+            a = b;
+            b = temp;
+        }
+        for (int i = a; i <= b; i++) {
+            if ( PerfectRoot(i) ) count++;
+        }
+        return count;
+    }
+    
+    private static boolean PerfectRoot(int num) {
+        final int sqrt = (int) Math.sqrt(num);
+        return  sqrt*sqrt == num;
+    }
+    
+}
